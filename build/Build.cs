57a1cbdd349a0488f3d772c59b41f410664ab018
Andrew DePersio <adepersio@developertown.com>
2022-11-26 01:10:59 -0500

    Initial commit

---
+++ build/Build.cs
@@ -0,0 +1,105 @@
+using Nuke.Common;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.IO.PathConstruction;
+
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main() => Execute<Build>(x => x.Publish);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter]
+    [Secret]
+    readonly string NuGetApiKey;
+
+    [Solution]
+    readonly Solution Solution;
+
+    [GitVersion]
+    readonly GitVersion GitVersion;
+
+    Target Clean => _ => _
+        .Before(Restore)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetClean(s => s
+                .SetProject(Solution)
+            );
+        });
+
+    Target Restore => _ => _
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetRestore(s => s
+                .SetProjectFile(Solution)
+            );
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+            );
+        });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetTest(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+            );
+        });
+
+    Target Pack => _ => _
+        .DependsOn(Test)
+        .Executes(() =>
+        {
+            DotNetTasks.DotNetPack(s => s
+                .SetProject(Solution)
+                .SetConfiguration(Configuration)
+                .SetVersion(GitVersion.NuGetVersionV2)
+                .SetOutputDirectory("artifacts")
+            );
+        });
+
+    Target Publish => _ => _
+        .DependsOn(Pack)
+        .Executes(() =>
+        {
+            GlobFiles("artifacts", "*.nupkg")
+                .ForEach(x =>
+                    {
+                        if (IsLocalBuild)
+                        {
+                            DotNetTasks.DotNetNuGetPush(s => s
+                                .SetTargetPath(x)
+                                .SetSource("C:/NuGet")
+                            );
+                        }
+                        else
+                        {
+                            DotNetTasks.DotNetNuGetPush(s => s
+                                .SetTargetPath(x)
+                                .SetSource("https://api.nuget.org/v3/index.json")
+                                .SetApiKey(NuGetApiKey)
+                            );
+                        }
+                    }
+                );
+        });
+}
---
+++ src/ExampleLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace ExampleLibrary.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void AddTest()
+        {
+            var calculator = new Calculator();
+            var results = calculator.Add(2, 2);
+            Assert.Equal(4, results);
+        }
+    }
+}
